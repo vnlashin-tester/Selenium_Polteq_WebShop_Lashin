eb153c4707a35e9194b18f85e4d4b449edcd311d
vnlashin-tester <vnlashin.tester@gmail.com>
2024-06-29 19:34:00 +0200

    add complex_test and fixtures

--- conftest.py
+++ conftest.py
@@ -11,12 +11,43 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 @pytest.fixture
 def driver():
-    # ARRANGE, executed before the test starts
+    # Driver srttings
     driver = webdriver.Chrome()
     driver.get("https://testshop.polteq-testing.com")
     driver.maximize_window()
     driver.implicitly_wait(5)
     yield driver  
     driver.quit()
+
+@pytest.fixture
+def login_fixture(driver): 
+    # Data
+    login_email = "tester@test.com"
+    login_password = "1qazxsw2"
+    
+    # Autorisation
+    sign_in_main_page = driver.find_element(By.XPATH,  '//div[@id="_desktop_user_info"]')
+    sign_in_main_page.click()
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//header[@class="page-header"]')))
+    email_field_autorisation = driver.find_element(By.XPATH, '//div[@class="col-md-6"]/input[@type="email"]') 
+    email_field_autorisation.clear()
+    email_field_autorisation.send_keys(login_email)
+    password_field_autorisation = driver.find_element(By.XPATH, '//input[@type="password"]')
+    password_field_autorisation.clear()
+    password_field_autorisation.send_keys(login_password)
+    button_sighn_in_form_of_autorisation = driver.find_element(By.XPATH, '//button[@id="submit-login"]') 
+    button_sighn_in_form_of_autorisation.click()
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//header[@class="page-header"]')))
+
+@pytest.fixture
+def logout_fixture(driver): 
+    go_to_your_account = driver.find_element(By.XPATH, '//span[contains(text(), "Polteq Tester")]')
+    go_to_your_account.click()
+    sign_out = driver.find_element(By.XPATH, '//a[contains(text(), "Sign out")]')
+    sign_out.click()
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//header[@class="page-header"]')))
+
+
